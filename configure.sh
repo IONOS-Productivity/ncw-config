b6ba8de293a35164a4974adf7d561795252bc9db
Kai Henseler <kai.henseler@strato.de>
2025-07-22 10:43:10 +0200

    chore: enforce ionos theme
    Signed-off-by: Kai Henseler <kai.henseler@strato.de>

--- configure.sh
+++ configure.sh
@@ -69,6 +69,7 @@ configure_theming() {
 	execute_occ_command theming:config disable-user-theming yes
 	#execute_occ_command theming:config favicon "${FAVICON_DIR}/favicon.ico"
 	execute_occ_command config:app:set theming backgroundMime --value backgroundColor
+	execute_occ_command config:system:set enforce_theme --value ionos
 
 	# Set homepage URL if configured
 	_ionos_homepage=$(execute_occ_command config:system:get ionos_homepage)
