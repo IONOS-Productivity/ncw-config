9eaabebb48d6b9d8623e026975805620396c2df4
Misha M.-Kupriyanov <kupriyanov@strato.de>
2025-07-23 14:38:30 +0200

    feat: add partial configuration, linting and build scripts
    Signed-off-by: Misha M.-Kupriyanov <kupriyanov@strato.de>

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+# SPDX-FileCopyrightText: 2025 STRATO AG
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+require_once './vendor/autoload.php';
+
+use PhpCsFixer\Runner\Parallel\ParallelConfigFactory;
+use Nextcloud\CodingStandard\Config;
+
+$config = new Config();
+$config
+	->setParallelConfig(ParallelConfigFactory::detect())
+	->getFinder()
+	->ignoreVCSIgnored(true)
+	->notPath('composer')
+	->notPath('node_modules')
+	->notPath('vendor')
+	->in('configs')
+	->in(__DIR__);
+
+return $config;
