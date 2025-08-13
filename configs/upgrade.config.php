fd9eeb63e95f93dd8a27b916770b1a93bc5a0a14
Misha M.-Kupriyanov <kupriyanov@strato.de>
2025-08-13 16:11:03 +0200

    chore(config): update SPDX license comments to block format
    Signed-off-by: Misha M.-Kupriyanov <kupriyanov@strato.de>

--- configs/upgrade.config.php
+++ configs/upgrade.config.php
@@ -1,7 +1,9 @@
 <?php
 
-# SPDX-FileCopyrightText: 2025 STRATO AG
-# SPDX-License-Identifier: AGPL-3.0-or-later
+/**
+ * SPDX-FileCopyrightText: 2025 STRATO AG
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
 
 $CONFIG = [
 	'upgrade.disable-web' => true,
