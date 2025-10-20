5162388a8db19a4aa7964f6b46d3a40f7de5d7bc
Misha M.-Kupriyanov <kupriyanov@strato.de>
2025-10-20 13:24:54 +0200

    fix(configure.sh): correct spacing in app background color configuration command
    Signed-off-by: Misha M.-Kupriyanov <kupriyanov@strato.de>

--- configure.sh
+++ configure.sh
@@ -78,7 +78,7 @@ configure_theming() {
 	execute_occ_command theming:config imprintUrl " "
 	execute_occ_command theming:config privacyUrl " "
 	execute_occ_command theming:config primary_color "#003D8F"
-	execute_occ_command config:app:set --value "#ffffff"  -- theming background_color
+	execute_occ_command config:app:set --value "#ffffff" -- theming background_color
 	execute_occ_command theming:config disable-user-theming yes
 	execute_occ_command theming:config disable_admin_theming yes
 	#execute_occ_command theming:config favicon "${FAVICON_DIR}/favicon.ico"
