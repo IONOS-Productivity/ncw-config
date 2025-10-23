0b4f7b1bf13c9ad384da36bcf7318758d31d6c6a
Arsalan Ul Haq Sohni <arsalan-ul-haq.sohni@strato.de>
2025-10-23 17:06:15 +0200

    build: enable end_to_end_encryption app
    Signed-off-by: Arsalan Ul Haq Sohni <arsalan-ul-haq.sohni@strato.de>

--- configure.sh
+++ configure.sh
@@ -586,6 +586,7 @@ configure_apps() {
 	enable_app circles "Circles"
 	enable_app activity "Activity"
 	enable_app contacts "Contacts"
+	enable_app end_to_end_encryption "End-to-End Encryption"
 	enable_app mail "Mail"
 	enable_app notifications "Notifications"
 	enable_app tasks "Tasks"
