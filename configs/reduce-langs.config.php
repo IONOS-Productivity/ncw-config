c0c98337df7f03258ab3025876428337df8f56d6
Arsalan Ul Haq Sohni <arsalan-ul-haq.sohni@strato.de>
2026-03-02 16:48:07 +0100

    feat(config): add configuration to reduce supported languages
    Signed-off-by: Arsalan Ul Haq Sohni <arsalan-ul-haq.sohni@strato.de>

---
+++ configs/reduce-langs.config.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * SPDX-FileCopyrightText: 2026 STRATO GmbH
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+$CONFIG = [
+	'reduce_to_languages' => [
+		'en_GB',
+		'de_DE',
+		'es',
+		'fr',
+		'it',
+		'nl',
+		'sv',
+	],
+];
